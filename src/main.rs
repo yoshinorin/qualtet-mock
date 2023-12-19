11eb0343e4be4bc5a3bd9b5a718ae3d4a1305c9d
yoshinorin <yoshinorin.net@outlook.com>
2023-12-20 00:36:27 +0900

    feat(arthives): add `archives` mock endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, Error, HttpResponse, HttpServer, Responder};
+use std::fs::File;
+use std::io::prelude::*;
 
 #[get("/")]
 async fn root() -> impl Responder {
     HttpResponse::Ok().body("Hello Qualtet!!")
 }
 
+#[get("/archives")]
+async fn archives() -> Result<HttpResponse, Error> {
+    let mut file: File = File::open("./src/resources/archives.json")?;
+    let mut contents: String = String::new();
+    file.read_to_string(&mut contents)?;
+
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(contents))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(root))
+    HttpServer::new(|| App::new().service(root).service(archives))
         .bind(("127.0.0.1", 9002))?
         .run()
         .await
